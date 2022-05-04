00cb05e33776e9a5538c202c4c9997432b53e0b9
marcxavier7 <99259689+marcxavier7@users.noreply.github.com>
2022-05-04 19:16:55 +0200

    Añadir Tooltip
    Añadir Tooltip

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ d3.csv ("ev_mig.csv").then (function (datos){
     console.log (+(datos[1].Inmigración))
     console.log(new Date(datos[1].Año, datos[1].Periodo=="Semestre 1" ? 5 :  11, 30))*/
     
-    const linea = d3.line()
+    const linea_Emigracion = d3.line()
         .x(d => x(new Date(d.Año, d.Periodo=="Semestre 1" ? 5 :  11, 30)))
         .y(d => y(d.Emigración));
     
-        const linea2 = d3.line()
+    const linea_Inmigracion = d3.line()
         .x(d => x(new Date(d.Año, d.Periodo=="Semestre 1" ? 5 :  11, 30)))
         .y(d => y(d.Inmigración));
     
@@ -47,10 +47,56 @@ d3.csv ("ev_mig.csv").then (function (datos){
         
         //EJE Y
         var y = d3.scaleLinear()
-            .domain([0,d3.max(datos, function(d) {return +d.Emigración;})])
+            .domain([0,d3.max(datos, function(d) {return Math.max(+d.Emigración, +d.Inmigración);})])
             .range([height, 0]);
         elementoSVG.append("g")
             .call(d3.axisLeft(y));
+    
+        //Tooltip
+
+    
+        var tooltip = d3.select ("body")
+            .append("div")
+            .attr("class", "tooltip")
+        
+        //Borrar tooltip Línea Emigración
+        
+        function borrarTooltip_Emigracion(){
+            tooltip
+                .transition()
+                .style("opacity", 0) //Que no se muestre el tooltip
+        }
+    
+        //Pintar tooltip Línea Emigración
+    
+        function pintarTooltip_Emigracion(d){
+            tooltip
+                .text("Año: " + d.Año + ", Período: " + d.Periodo + ", Emigración: " + d.Emigración)
+                .style("top", d3.event.pageY + "px") //Te da la posición de la coordenada y donde se ha producido el evento, donde posicionar el tooltip
+                .style("left", d3.event.pageX + "px") //Te da la posición de la coordenada x donde se ha producido el evento
+                .transition()
+                .style("opacity",1) //Para mostrar el tooltip
+        }
+    
+        //Borrar tooltip Línea Inmigración
+    
+        function borrarTooltip_Inmigracion(){
+            tooltip
+                .transition()
+                .style("opacity", 0) //Que no se muestre el tooltip
+        }
+    
+        
+        //Pintar tooltip Línea Inmigración
+    
+        function pintarTooltip_Inmigracion(d){
+            tooltip
+                .text("Año: " + d.Año + ", Período: " + d.Periodo + ", Inmigración: " + d.Inmigración)
+                .style("top", d3.event.pageY + "px") //Te da la posición de la coordenada y donde se ha producido el evento, donde posicionar el tooltip
+                .style("left", d3.event.pageX + "px") //Te da la posición de la coordenada x donde se ha producido el evento
+                .transition()
+                .style("opacity",1) //Para mostrar el tooltip
+        }
         
         //Línea
         
@@ -59,14 +105,23 @@ d3.csv ("ev_mig.csv").then (function (datos){
             .attr("fill","none")
             .attr("stroke", "steelblue")
             .attr("stroke-width",1.5)
-            .attr("d",linea)
+            .attr("d",linea_Emigracion)
+            .on ("mouseover", d => {
+                pintarTooltip_Emigracion(d)
+            })
+            .on("mouseout", borrarTooltip_Emigracion)
     
-                elementoSVG.append("path")
+        elementoSVG.append("path")
             .datum(datos)
             .attr("fill","none")
             .attr("stroke", "red")
             .attr("stroke-width",1.5)
-            .attr("d",linea2)
+            .attr("d",linea_Inmigracion)
+            .on ("mouseover", d => {
+                pintarTooltip_Inmigracion(d)
+            })
+            .on("mouseout", borrarTooltip_Inmigracion)
+    
     })
     
     
